303e02a3eaaad99d1f57c7fca3e98526e391addb
Allan Julie Fontes de Oliveira <allanzinhofontes@gmail.com>
2021-12-22 21:35:26 -0300

    feat(home-styles): styles added to home page

--- project/src/pages/index.tsx
+++ project/src/pages/index.tsx
@@ -29,20 +29,26 @@ function Home() {
         <button className={styles.addPostButton}>
           <Link href={"/posts/createAPost"} prefetch>
             <div>
-              <MdAdd className={styles.addIcon} />
               <span>New post</span>
+              <MdAdd className={styles.addIcon} />
             </div>
           </Link>
         </button>
       </nav>
 
-      <h3>{`${generateTimeMessage()} ${
-        user.displayName ? user.displayName : ""
-      }`}</h3>
+      <div className={styles.container}>
+        <h3>{`${generateTimeMessage()} ${
+          user.displayName ? user.displayName : ""
+        }`}</h3>
 
-      <div className={styles.postsWrapper}>
-        <AiOutlineFileAdd className={styles.addIcon} />
-        <h4>Add posts to your collection!</h4>
+        <div className={styles.postsWrapper}>
+          <Link href={"/posts/createAPost"} prefetch>
+            <a>
+              <AiOutlineFileAdd className={styles.addIcon} />
+              <h4>Add posts to your collection!</h4>
+            </a>
+          </Link>
+        </div>
       </div>
     </>
   );
