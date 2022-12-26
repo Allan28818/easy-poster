9681a2259801741e8e83e16b378c1de09d0933ee
Allan Julie Fontes de Oliveira <allanzinhofontes@gmail.com>
2022-12-26 14:56:57 -0300

    feat(functions-working): functions working in the application

--- project/functions/src/index.ts
+++ project/functions/src/index.ts
@@ -1,25 +1,25 @@
 import * as functions from "firebase-functions";
 
-import admin from "firebase-admin";
+import { credential, initializeApp, firestore } from "firebase-admin";
 import { onFollowUserModuleFunction } from "./functionsCode/onFollowUserModule";
 import { onUnfollowUserModuleFunction } from "./functionsCode/onUnfollowUserModule";
 import { PropsReturn } from "./models/core.response";
 const serviceAccount = require("../service-account.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+initializeApp({
+  credential: credential.cert(serviceAccount),
 });
 
-const db = admin.firestore();
+const db = firestore();
 
 export const onFollowUser = functions.https.onCall(
   async (data, context): Promise<PropsReturn> => {
     if (!context.auth) {
-      throw new Error("User not authenticated (cotext.auth = undfined)");
+      throw new Error("User not authenticated (context.auth = undfined)");
     }
     const response = await onFollowUserModuleFunction({
-      newFollowerId: data.newFollowerId,
-      userFollowedId: data.userFollowedId,
+      newFollowerId: data.body.newFollowerId,
+      userFollowedId: data.body.userFollowedId,
       db,
     });
 
@@ -30,12 +30,12 @@ export const onFollowUser = functions.https.onCall(
 export const onUnfollowUser = functions.https.onCall(
   async (data, context): Promise<PropsReturn> => {
     if (!context.auth) {
-      throw new Error("User not authenticated (cotext.auth = undfined)");
+      throw new Error("User not authenticated (context.auth = undfined)");
     }
 
     const response = await onUnfollowUserModuleFunction({
-      accountToUnfollowId: data.accountToUnfollowId,
-      unfollowRequesterId: data.unfollowRequesterId,
+      accountToUnfollowId: data.body.accountToUnfollowId,
+      unfollowRequesterId: data.body.unfollowRequesterId,
       db,
     });
 
