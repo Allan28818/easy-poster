ab7fdef219aa8f177d5535e11f0d4d2c48974285
Allan Julie Fontes de Oliveira <allanzinhofontes@gmail.com>
2023-07-30 21:47:42 -0300

    feat(simple-icons-styles): simple icon styles finished

--- project/src/components/Toolbars/LexicalToolbar.tsx
+++ project/src/components/Toolbars/LexicalToolbar.tsx
@@ -1,22 +1,24 @@
 import Image from "next/image";
 
-import styles from "../../styles/components/toolbars/lexical-toolbar.module.scss";
-import { AiFillStar, AiOutlineStar } from "react-icons/ai";
-import { FaItalic, FaLock, FaRedo, FaUndo } from "react-icons/fa";
-import { BsGlobe2, BsListUl } from "react-icons/bs";
-import { FiUser } from "react-icons/fi";
+import { AiFillStar, AiOutlinePlus, AiOutlineStar } from "react-icons/ai";
 import { BiListPlus, BiSolidImageAdd } from "react-icons/bi";
-import { RxFontBold, RxText, RxTextNone } from "react-icons/rx";
+import { BsGlobe2, BsListUl } from "react-icons/bs";
+import { FaItalic, FaLock, FaRedo, FaUndo } from "react-icons/fa";
+import { FiUser } from "react-icons/fi";
+import { GrTextAlignFull } from "react-icons/gr";
 import {
   ImIndentDecrease,
   ImIndentIncrease,
   ImUnderline,
 } from "react-icons/im";
-import { LuPaintBucket } from "react-icons/lu";
 import { IoLinkSharp } from "react-icons/io5";
+import { LuPaintBucket } from "react-icons/lu";
 import { MdOutlineInsertComment } from "react-icons/md";
-import { GrTextAlignFull } from "react-icons/gr";
 import { PiListChecksBold } from "react-icons/pi";
+import { RiSubtractFill } from "react-icons/ri";
+import { RxFontBold, RxText, RxTextNone } from "react-icons/rx";
+import { VscAdd } from "react-icons/vsc";
+import styles from "../../styles/components/toolbars/lexical-toolbar.module.scss";
 
 interface LexicalToolbarProps {
   isFavorite: boolean;
@@ -96,15 +98,15 @@ const LexicalToolbar = (props: LexicalToolbarProps) => {
           <div className={styles.primaryTools}>
             <div className={styles.toolGroup}>
               <button className={styles.toolButton}>
-                <FaUndo className={styles.toolButtonIcon} />
+                <FaUndo className={styles.historyButtons} />
               </button>
               <button className={styles.toolButton}>
-                <FaRedo className={styles.toolButtonIcon} />
+                <FaRedo className={styles.historyButtons} />
               </button>
             </div>
             <div className={styles.toolGroup}>
               <button className={styles.toolButtonSelect}>
-                <BiListPlus className={styles.toolButtonIcon} />
+                <BiListPlus className={styles.fontSizeIcon} />
                 <select className={styles.toolOptions}>
                   <option>Normal</option>
                 </select>
@@ -112,7 +114,7 @@ const LexicalToolbar = (props: LexicalToolbarProps) => {
             </div>
             <div className={styles.toolGroup}>
               <button className={styles.toolButtonSelect}>
-                <RxText className={styles.toolButtonIcon} />
+                <RxText className={styles.fontFamilyIcon} />
                 <select className={styles.toolOptions}>
                   <option>
                     <p>Regular</p>
@@ -121,64 +123,70 @@ const LexicalToolbar = (props: LexicalToolbarProps) => {
               </button>
             </div>
             <div className={styles.toolGroup}>
+              <button className={styles.fontSizeButton}>
+                <RiSubtractFill className={styles.fontSizeButtonIcon} />
+              </button>
               <input className={styles.fontSizeInput} type="number" min={1} />
+              <button className={styles.fontSizeButton}>
+                <AiOutlinePlus className={styles.fontSizeButtonIcon} />
+              </button>
             </div>
             <div className={styles.toolGroup}>
               <button className={styles.toolButton}>
-                <RxFontBold className={styles.toolButtonIcon} />
+                <RxFontBold className={styles.boldIcon} />
               </button>
               <button className={styles.toolButton}>
-                <FaItalic className={styles.toolButtonIcon} />
+                <FaItalic className={styles.italicIcon} />
               </button>
               <button className={styles.toolButton}>
-                <ImUnderline className={styles.toolButtonIcon} />
+                <ImUnderline className={styles.underlineIcon} />
               </button>
-              <button className={styles.toolButton}>
+              <button className={styles.colorToolButton}>
                 <p className={styles.textColorTool}>A</p>
                 <div className={styles.textColorIndicator} />
               </button>
-              <button className={styles.toolButton}>
-                <LuPaintBucket className={styles.toolIcon} />
+              <button className={styles.colorToolButton}>
+                <LuPaintBucket className={styles.inkBucket} />
                 <div className={styles.textColorIndicator} />
               </button>
             </div>
             <div className={styles.toolGroup}>
               <button className={styles.toolButton}>
-                <IoLinkSharp className={styles.toolButtonIcon} />
+                <IoLinkSharp className={styles.linkIcon} />
               </button>
               <button className={styles.toolButton}>
-                <BiSolidImageAdd className={styles.toolButtonIcon} />
+                <BiSolidImageAdd className={styles.imageIcon} />
               </button>
               <button className={styles.toolButton}>
-                <MdOutlineInsertComment className={styles.toolButtonIcon} />
+                <MdOutlineInsertComment className={styles.commentIcon} />
               </button>
             </div>
             <div className={styles.toolGroup}>
               <button className={styles.toolButtonSelect}>
                 <select className={styles.toolOptions}>
                   <option>
-                    <GrTextAlignFull className={styles.toolButtonIcon} />
+                    <GrTextAlignFull className={styles.alignTextIcon} />
                   </option>
                 </select>
               </button>
               <button className={styles.toolButton}>
-                <PiListChecksBold className={styles.toolButtonIcon} />
+                <PiListChecksBold className={styles.checkListIcon} />
               </button>
               <button className={styles.toolButtonSelect}>
                 <select className={styles.toolOptions}>
                   <option>
-                    <BsListUl className={styles.toolButtonIcon} />
+                    <BsListUl className={styles.listIcon} />
                   </option>
                 </select>
               </button>
               <button className={styles.toolButton}>
-                <ImIndentDecrease className={styles.toolButtonIcon} />
+                <ImIndentDecrease className={styles.indentIcon} />
               </button>
               <button className={styles.toolButton}>
-                <ImIndentIncrease className={styles.toolButtonIcon} />
+                <ImIndentIncrease className={styles.indentIcon} />
               </button>
               <button className={styles.toolButton}>
-                <RxTextNone className={styles.toolButtonIcon} />
+                <RxTextNone className={styles.clearTextIcon} />
               </button>
             </div>
           </div>
